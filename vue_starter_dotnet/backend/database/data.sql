b5300b9a7ef281412527c96bb795942bb3dcc00a
Chris Jourden <cjourden410@gmail.com>
2020-04-07 10:16:23 -0400

    Added Brewery table to script, added an admin and brewer to users scrpit

--- vue_starter_dotnet/backend/database/data.sql
+++ vue_starter_dotnet/backend/database/data.sql
@@ -10,4 +10,22 @@ INSERT INTO users
 VALUES
   ('user', 'jUE98uhvS5tdIlxRsmz1W7/Qaqo=', '9CWPUTvXqQ4=', 'User');
 
+-- default username of 'admin' and default password of 'password'
+INSERT INTO users
+  (username,password,salt,role)
+VALUES
+  ('admin', '+LzIkKQWvUXKHIp+oTB77lJu9V0=', 'Ce1wvvfHcpg=', 'Admin');
+
+-- default username of 'Mike' and default password of 'password'
+INSERT INTO users
+  (username,password,salt,role)
+VALUES
+  ('Mike', 'INZ+8Cfe3TLRO5lM2skBfWr7Wkc=', 'K5ZEctZBc+g=', 'Brewer');
+
+-- adding brewery to breweries table
+INSERT INTO breweries
+  (name,userID,username)
+VALUES
+  ('Great Lakes Brewing Company', 3, 'Mike');
+
 COMMIT TRANSACTION;
