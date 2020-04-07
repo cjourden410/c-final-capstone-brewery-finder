9a0e9530e197f5d662e398561f0d74fa378d9206
frederick stewart <stewart1130@gmail.com>
2020-04-07 13:05:02 -0400

    push for merge conf prevention

--- vue_starter_dotnet/frontend/src/views/BreweryAdd.vue
+++ vue_starter_dotnet/frontend/src/views/BreweryAdd.vue
@@ -1,30 +1,73 @@
 <template>
   <div class="BreweryAdd">
     <h1>Add Brewery</h1>
-    <form >
-    <div>Name of Brewery: <input type="text" name="Brewery" v-model="Brewery" placeholder="Billy Bob's Shine Factory" /></div>
-    <div>Name of Head Brewer: <input placeholder="Billy Bob" /></div>
-    <div>User ID: <input placeholder="123" /></div>
-    <div>Username: <input placeholder="BillyBob123" /></div>
-    <button type="submit" @click.stop.prevent="submit()">Submit</button>
+    <form>
+      <div>
+        Name of Brewery:
+        <input type="text" name="Brewery" v-model="Brewery" placeholder="Billy Bob's Shine Factory" />
+      </div>
+      <div>
+        Name of Head Brewer:
+        <input placeholder="Billy Bob" />
+      </div>
+      <div>
+        User ID:
+        <input placeholder="123" />
+      </div>
+      <div>
+        Username:
+        <input placeholder="BillyBob123" />
+      </div>
+      <button type="submit" @click.stop.prevent="submit()">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-      return {
-        Brewery : null
-      }
+  name: "breweryAdd",
+  data() {
+    return {
+      user: {
+        nameOfBrewery: "",
+        HeadBrewer: "",
+        UserID: "",
+        Username: ""
+      },
+      registrationErrors: false
+    };
+  },
+  methods: {
+    submit() {
+      //if you want to send any data into server before redirection then you can do it here
+
+      this.$router.push("/BreweryList");
     },
-    methods: {
-      submit(){
-         //if you want to send any data into server before redirection then you can do it here
-        this.$router.push("/BreweryList");
-      }
+    register() {
+      fetch(`${process.env.VUE_APP_REMOTE_API}/breweryadd`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.brewery)
+      })
+        .then(response => {
+          if (response.ok) {
+            this.$router.push({
+              path: "/breweryAdd",
+              query: { registration: "success" }
+            });
+          } else {
+            this.registrationErrors = true;
+          }
+        })
+
+        .then(err => console.error(err));
     }
   }
+};
+
 </script>
 
 <style>
@@ -36,7 +79,7 @@ export default {
   padding-bottom: 10px;
   text-align: center;
 }
-input{
+input {
   width: 20%;
 }
 </style>
