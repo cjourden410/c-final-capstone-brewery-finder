25a409fbf4f9c64eca10c8bae16e758be0c29c42
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-04-08 15:21:54 -0400

    add beer stuff

--- vue_starter_dotnet/frontend/src/views/BeerAdd.vue
+++ vue_starter_dotnet/frontend/src/views/BeerAdd.vue
@@ -1,6 +1,28 @@
-<template>
-  <div>
-    
+<template v-if="user.rol==='Brewer'">
+  <div class="BeerAdd">
+    <h1>Add a New Beer</h1>
+    <form>
+      <div>
+        Name of Beer:
+        <input type="text" v-model="beer.name" class="beerInput" placeholder="Bob's Beer" />
+      </div>
+      <div>
+        Discription of Beer:
+        <input type="text" v-model="beer.discription" class="beerInput" placeholder="Stuff about your beer" />
+      </div>
+      <div>
+        Type of Beer:
+        <input type="text" v-model="beer.type" class="beerInput" placeholder="Lager" />
+      </div>
+       <div>
+        ABV of Beer:
+        <input type="text" v-model="beer.abv" class="beerInput" placeholder="9.0" />
+      </div>
+       <div>
+        Your Brewery:
+        <input type="text" v-model="beer.brewery" class="beerInput" placeholder="Bob's Brewery" />
+      </div>
+    </form>
   </div>
 </template>
 
@@ -8,8 +30,48 @@
 import auth from "@/auth.js";
 
 export default {
+  name: "beerAdd",
+  props: {},
+  data(){
+    return{
+      beer:{
+        name: "",
+        discription: "",
+        type: "",
+        abv: "",
+        brewery: ""
+      },
+      registrationErrors: false
+    };
+  },
+  created(){
+    this.user = auth.getUser();
+  },
+  methods:{
+    addBeer(){
+          let url = `${process.env.VUE_APP_REMOTE_API}/breweries`;
 
-}
+      fetch(url,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.beer)
+      }).then(response => {
+        if(response.ok){
+          response.json().then(json => {
+            this.beer = json;
+            alert(`Beer ${this.beer.name} was added!`)
+          });
+        }
+        else{
+          alert(`There was an error: ${response.status}: ${response.statusText}`)
+        }
+      });
+    }
+
+  }
+};
 </script>
 
 <style>
