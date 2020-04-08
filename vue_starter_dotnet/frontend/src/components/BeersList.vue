bc5106d1202a1f7d99803194028cd364e4cdff7f
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-04-08 11:50:05 -0400

    in progress

---
+++ vue_starter_dotnet/frontend/src/components/BeersList.vue
@@ -0,0 +1,21 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Type></td>
+                <td>ABV</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="beer in beers" :key="beer.id">
+                <td>
+                    <router-link :to="{name: 'Beer', params:{id:beer.id}}">{{beer.id}}</router-link>
+                </td>
+                <td>{{beer.type}}</td>
+                <td>{{beer.abv}}</td>
+        </tbody>
+    </table>
+</template>
+<script>
+</script>
---
+++ vue_starter_dotnet/frontend/src/views/BeerList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="beerList">
+      <h1>Beer List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
