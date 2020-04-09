673369e0fc46e0d7b73868e31e16b9c35bc0edc5
Chris Jourden <cjourden410@gmail.com>
2020-04-09 10:50:02 -0400

    Beer model created, Beer Controller created, GetBeers, GetBeer by id and Add Beer added to Beer Controller and Brewery DAO(IDAO) respectively.

---
+++ vue_starter_dotnet/backend/SampleApi/Models/Beer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Models
+{
+    public class Beer
+    {
+        /// <summary>
+        /// The beers id.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The beers name.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The beers description.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// The beers image.
+        /// </summary>
+        public string Image { get; set; }
+        /// <summary>
+        /// The beers abv.
+        /// </summary>
+        public string ABV { get; set; }
+        /// <summary>
+        /// The beers type.
+        /// </summary>
+        public string BeerType { get; set; }
+        /// <summary>
+        /// The brewerys Id to connect beer to a brewery.
+        /// </summary>
+        public int BreweryID { get; set; }
+    }
+}
