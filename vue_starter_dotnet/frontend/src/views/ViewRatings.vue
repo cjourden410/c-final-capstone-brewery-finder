e698b948be696da6e406a39d5d324e046f90bdbb
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-04-10 14:23:41 -0400

    help Fredgit add -Agit add -Agit add -A

--- vue_starter_dotnet/frontend/src/views/ViewRatings.vue
+++ vue_starter_dotnet/frontend/src/views/ViewRatings.vue
@@ -1,44 +1,86 @@
 <template>
   <div class="base">
-      <div class ="viewRatings">
+    <div class="viewRatings">
       <h1>Veiw Beer Ratings</h1>
-      <rating-list :rating="rating" class="rating"></rating-list>
-      </div>
+      <form>
+        <div>
+          Brewery Name:
+          <select @change="getBrewery(chosenId)" v-model="chosenId">
+            <option
+              v-for="brewery in breweries"
+              :key="brewery.id"
+              v-bind:value="brewery.id"
+            >{{brewery.name}}</option>
+          </select>
+        </div>
+        <div v-if="selectedBrewery" class="infoList">
+          <rating-list :rating="rating" class="rating"></rating-list>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import RatingsList from "@/components/RatingsList.vue";
 export default {
-name: 'rating',
-components:{
+  name: "rating",
+  components: {
     "rating-list": RatingsList
-},
-data(){
-    return{
-        ratings:[]
-    }
-},
-methods:{
-    GetRatings(breweryID){
-    let url =`${process.env.VUE_APP_REMOTE_API}/beers?breweryID=${breweryID}`;
-    fetch(url)
-    .then(response => {
-        response.json()
-        .then(json => {
-            this.ratings = json
+  },
+  data() {
+    return {
+      ratings: [],
+      selectedBrewery: null,
+      breweries: [],
+      chosenId: Number
+    };
+  },
+  methods: {
+    GetBreweries() {
+      let url = `${process.env.VUE_APP_REMOTE_API}/breweries`;
+
+      fetch(url)
+        .then(response => {
+          response.json().then(json => {
+            this.breweries = json;
+          });
         })
-    }).catch(err => {
-        console.log(err)
-    });
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    getBrewery(id) {
+      let url = `${process.env.VUE_APP_REMOTE_API}/breweries/${id}`;
+      fetch(url)
+        .then(response => {
+          response.json().then(json => {
+            this.selectedBrewery = json;
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    GetRatings(breweryID) {
+      let url = `${process.env.VUE_APP_REMOTE_API}/reviews?breweryID=${breweryID}`;
+      fetch(url)
+        .then(response => {
+          response.json().then(json => {
+            this.ratings = json;
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-},
-created(){
+  },
+  created() {
     this.user = this.$attrs.user;
     this.GetRatings(this.$route.params.id);
-}
-}
-
+    this.getBreweries();
+  }
+};
 </script>
 
 <style>
@@ -51,12 +93,12 @@ created(){
   padding-bottom: 20px;
   text-align: center;
 }
-.rating{
-    margin: auto;
+.rating {
+  margin: auto;
 }
-.base{
+.base {
   width: auto;
   height: 600px;
-  background-image: url('../assets/13.jpg');
+  background-image: url("../assets/13.jpg");
 }
 </style>
