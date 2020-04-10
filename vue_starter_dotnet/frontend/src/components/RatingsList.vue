db3f6661ad9add764a40248d04e787746640f909
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-04-10 10:16:52 -0400

    I love CSS part II

---
+++ vue_starter_dotnet/frontend/src/components/RatingsList.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <h1>Your Brewery Ratings and Reviews</h1>
+  <table>
+      <thead>
+          <tr>
+              <td>Beer Name</td>
+              <td>Beer Rating</td>
+              <td>Comments</td>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for="rating in ratings" :key="rating.id">
+              <td>{{rating.beerName}}</td>
+              <td>{{rating.beerRating}}</td>
+              <td>{{rating.comments}}</td>
+          </tr>
+      </tbody>
+  </table>
+  </div>
+</template>
+
+<script>
+export default {
+name: "rating-list",
+props:{
+    ratings: Array
+},
+methods: {},
+computed: {}
+};
+</script>
+
+<style>
+
+</style>
--- vue_starter_dotnet/frontend/src/views/ViewRatings.vue
+++ vue_starter_dotnet/frontend/src/views/ViewRatings.vue
@@ -2,14 +2,42 @@
   <div class="base">
       <div class ="viewRatings">
       <h1>Veiw Beer Ratings</h1>
+      <rating-list :rating="rating" class="rating"></rating-list>
       </div>
   </div>
 </template>
 
 <script>
+import RatingsList from "@/components/RatingsList.vue";
 export default {
-
+name: 'rating',
+components:{
+    "rating-list": RatingsList
+},
+data(){
+    return{
+        ratings:[]
+    }
+},
+methods:{
+    GetRatings(breweryID){
+    let url =`${process.env.VUE_APP_REMOTE_API}/beers?breweryID=${breweryID}`;
+    fetch(url)
+    .then(response => {
+        response.json()
+        .then(json => {
+            this.ratings = json
+        })
+    }).catch(err => {
+        console.log(err)
+    });
+    }
+},
+created(){
+    this.GetRatings(this.$route.params.id);
 }
+}
+
 </script>
 
 <style>
@@ -20,6 +48,9 @@ export default {
   margin-right: 15%;
   padding-bottom: 10px;
   text-align: center;
+}
+.rating{
+    margin: auto;
 }
 .base{
   width: auto;
