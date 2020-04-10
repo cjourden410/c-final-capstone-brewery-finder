bad22af8bbf532c9b797c772737b52ac3f0a2d7d
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-04-10 11:08:11 -0400

    working on it

--- vue_starter_dotnet/frontend/src/views/BeerDelete.vue
+++ vue_starter_dotnet/frontend/src/views/BeerDelete.vue
@@ -1,6 +1,8 @@
 <template>
       <div>
             <h1>Remove this swill from your Beer List</h1>
+            <button v-on:click="deleteBeer">Delete Beer</button>
+            <router-link tag="button" :to="home">Cancel</router-link>
       </div>
 
 </template>
